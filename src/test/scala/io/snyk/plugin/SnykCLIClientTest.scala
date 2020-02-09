8215e400087cc9eeca5b47fa3575aac7db0d2f10
Aleksandr <aldanchenko@gmail.com>
2020-02-09 17:55:17 +0200

    Test: merge two test files to one.

--- src/test/scala/io/snyk/plugin/SnykCLIClientTest.scala
+++ src/test/scala/io/snyk/plugin/SnykCLIClientTest.scala
@@ -1,10 +1,37 @@
 package io.snyk.plugin
 
 import io.snyk.plugin.client.{SnykCLIClient, SnykConfig}
+import io.snyk.plugin.datamodel.{SecurityVuln, SnykMavenArtifact}
+import io.snyk.plugin.ui.state.SnykPluginState
+import org.junit.Assert.{assertEquals, assertNotNull, assertTrue}
 import org.junit.Test
-import org.junit.Assert._
 
-class SnykCLIClientTest {
+import scala.io.{Codec, Source}
+
+class SnykCLIClientTest extends AbstractMavenTestCase() {
+
+  override protected def setUp(): Unit = {
+    super.setUp()
+
+    val projectXmlStr = Source.fromResource("sample-pom.xml", getClass.getClassLoader)(Codec.UTF8).mkString
+
+    importProject(projectXmlStr)
+  }
+
+  @Test
+  def testRunScan(): Unit = {
+    val snykPluginState = SnykPluginState.forIntelliJ(currentProject)
+
+    val triedSnykVulnResponse = snykPluginState.apiClient.runScan(currentProject, SnykMavenArtifact.empty)
+
+    assertTrue(triedSnykVulnResponse.isSuccess)
+
+    val vulnerabilities = triedSnykVulnResponse.get.vulnerabilities
+
+    assertEquals("One vulnerability expected", 1, vulnerabilities.size)
+    assertEquals("org.codehaus.jackson:jackson-mapper-asl",
+      vulnerabilities.head.asInstanceOf[SecurityVuln].moduleName)
+  }
 
   @Test
   def testUserInfoEndpoint(): Unit = {
