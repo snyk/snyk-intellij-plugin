6d8f230c35b0323cbdc78d292061ddc50447bd27
Pavel Sorokin <60606414+pavel-snyk@users.noreply.github.com>
2020-08-07 16:54:24 +0200

    Convert build groovy scripts to kotlin based
    - update Gradle version to 6.5.1
    - use modified JetBrains platform plugin

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+import org.jetbrains.changelog.closure
+import org.jetbrains.changelog.markdownToHTML
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  id("org.jetbrains.changelog") version "0.4.0"
+  id("org.jetbrains.intellij") version "0.4.21"
+  id("org.jetbrains.kotlin.jvm") version "1.3.72"
+}
+
+// variables from gradle.properties file
+val pluginVersion: String by project
+
+group = "io.snyk.intellij"
+description = "Snyk Vulnerability Scanner"
+version = pluginVersion
+
+dependencies {
+  implementation("org.jetbrains.kotlin:kotlin-stdlib")
+}
+
+repositories {
+  jcenter()
+}
+
+tasks {
+  withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.languageVersion = "1.3"
+  }
+
+  buildSearchableOptions {
+    enabled = false
+  }
+
+  patchPluginXml {
+    version(pluginVersion)
+
+    pluginDescription(closure {
+      File("./README.md").readText().lines().run {
+        val start = "<!-- Plugin description start -->"
+        val end = "<!-- Plugin description end -->"
+
+        if (!containsAll(listOf(start, end))) {
+          throw GradleException("Plugin description section not found in README.md file:\n$start ... $end")
+        }
+        subList(indexOf(start) + 1, indexOf(end))
+      }.joinToString("\n").run { markdownToHTML(this) }
+    })
+  }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "snyk-intellij-plugin"
