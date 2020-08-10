172649d38d4c3ae595c1ac205e00901aebb01138
Aleksandr <aldanchenko@gmail.com>
2020-08-10 14:12:09 +0300

    feat: improved UI.

--- src/main/scala/io/snyk/plugin/ui/settings/SettingsDialog.scala
+++ src/main/scala/io/snyk/plugin/ui/settings/SettingsDialog.scala
@@ -1,13 +1,13 @@
 package io.snyk.plugin.ui.settings
 
 import io.snyk.plugin.Utils.isUrlValid
-
 import java.util.Objects.nonNull
 import java.awt.{Dimension, Insets}
 
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.ui.{ComponentValidator, ValidationInfo}
-import com.intellij.ui.DocumentAdapter
+import com.intellij.ui.components.fields.ExpandableTextField
+import com.intellij.ui.{DocumentAdapter, IdeBorderFactory}
 import com.intellij.uiDesigner.core.{Spacer, GridConstraints => UIGridConstraints, GridLayoutManager => UIGridLayoutManager}
 import javax.swing._
 import javax.swing.event.DocumentEvent
@@ -21,7 +21,7 @@ class SettingsDialog(applicationSettings: SnykApplicationSettingsStateService,
 
   private val organizationTextField: JTextField = new JTextField()
   private val ignoreUnknownCACheckBox: JCheckBox = new JCheckBox()
-  private val additionalParametersTextField: JTextField = new JTextField("")
+  private val additionalParametersTextField: JTextField = new ExpandableTextField()
 
   private val rootPanel: JPanel = new JPanel()
 
@@ -45,115 +45,98 @@ class SettingsDialog(applicationSettings: SnykApplicationSettingsStateService,
   }
 
   private def setupUI(): Unit = {
-    val gridLayoutManager = new UIGridLayoutManager(6, 2, new Insets(0, 0, 0, 0), -1, -1)
+    val defaultTextFieldWidth = 500
 
-    rootPanel.setLayout(gridLayoutManager)
+    rootPanel.setLayout(
+      new UIGridLayoutManager(3, 1, new Insets(0, 0, 0, 0), -1, -1))
 
-    val customEndpointLabel = new JLabel
-    customEndpointLabel.setText("Custom endpoint:")
+    val generalSettingsPanel = new JPanel(new UIGridLayoutManager(4, 3, new Insets(0, 0, 0, 0), -1, -1))
+    generalSettingsPanel.setBorder(IdeBorderFactory.createTitledBorder("General settings"))
 
-    rootPanel.add(customEndpointLabel, new UIGridConstraints(
-      0,
-      0,
-      1,
-      1,
-      UIGridConstraints.ANCHOR_WEST,
-      UIGridConstraints.FILL_NONE,
-      UIGridConstraints.SIZEPOLICY_FIXED,
-      UIGridConstraints.SIZEPOLICY_FIXED,
-      null,
-      new Dimension(110, 16),
-      null,
-      0,
-      false))
+    rootPanel.add(
+      generalSettingsPanel,
+      new UIGridConstraints(
+        0,
+        0,
+        1,
+        1,
+        UIGridConstraints.ANCHOR_CENTER,
+        UIGridConstraints.FILL_BOTH,
+        UIGridConstraints.SIZEPOLICY_CAN_SHRINK | UIGridConstraints.SIZEPOLICY_CAN_GROW,
+        UIGridConstraints.SIZEPOLICY_CAN_SHRINK | UIGridConstraints.SIZEPOLICY_CAN_GROW,
+        null,
+        new Dimension(200, 200),
+        null,
+        0,
+        false
+      )
+    )
 
-    val spacer = new Spacer()
+    val customEndpointLabel = new JLabel("Custom endpoint:")
 
-    rootPanel.add(spacer, new UIGridConstraints(
-      5,
-      0,
-      1,
-      2,
-      UIGridConstraints.ANCHOR_CENTER,
-      UIGridConstraints.FILL_VERTICAL,
-      1,
-      UIGridConstraints.SIZEPOLICY_WANT_GROW,
-      null,
-      new Dimension(110, 14),
-      null,
-      0,
-      false))
+    generalSettingsPanel.add(
+      customEndpointLabel,
+      new UIGridConstraints(
+        0,
+        0,
+        1,
+        1,
+        UIGridConstraints.ANCHOR_WEST,
+        UIGridConstraints.FILL_NONE,
+        UIGridConstraints.SIZEPOLICY_FIXED,
+        UIGridConstraints.SIZEPOLICY_FIXED,
+        null,
+        new Dimension(120, 16),
+        null,
+        0,
+        false
+      )
+    )
 
-    rootPanel.add(customEndpointTextField, new UIGridConstraints(
-      0,
-      1,
-      1,
-      1,
-      UIGridConstraints.ANCHOR_WEST,
-      UIGridConstraints.FILL_HORIZONTAL,
-      UIGridConstraints.SIZEPOLICY_WANT_GROW,
-      UIGridConstraints.SIZEPOLICY_FIXED,
-      null,
-      new Dimension(150, -1),
-      null,
-      0,
-      false))
-
-    val organizationLabel = new JLabel
-    organizationLabel.setText("Organization:")
-
-    rootPanel.add(organizationLabel, new UIGridConstraints(
-      2,
-      0,
-      1,
-      1,
-      UIGridConstraints.ANCHOR_WEST,
-      UIGridConstraints.FILL_NONE,
-      UIGridConstraints.SIZEPOLICY_FIXED,
-      UIGridConstraints.SIZEPOLICY_FIXED,
-      null,
-      null,
-      null,
-      0,
-      false))
-    rootPanel.add(organizationTextField, new UIGridConstraints(
-      2,
-      1,
-      1,
-      1,
-      UIGridConstraints.ANCHOR_WEST,
-      UIGridConstraints.FILL_HORIZONTAL,
-      UIGridConstraints.SIZEPOLICY_WANT_GROW,
-      UIGridConstraints.SIZEPOLICY_FIXED,
-      null,
-      new Dimension(150, -1),
-      null,
-      0,
-      false))
+    generalSettingsPanel.add(
+      customEndpointTextField,
+      new UIGridConstraints(
+        0,
+        1,
+        1,
+        1,
+        UIGridConstraints.ANCHOR_WEST,
+        UIGridConstraints.SIZEPOLICY_FIXED,
+        UIGridConstraints.SIZEPOLICY_FIXED,
+        UIGridConstraints.SIZEPOLICY_FIXED,
+        null,
+        new Dimension(defaultTextFieldWidth, -1),
+        null,
+        0,
+        false
+      )
+    )
 
     ignoreUnknownCACheckBox.setText("Ignore unknown CA")
+    generalSettingsPanel.add(
+      ignoreUnknownCACheckBox,
+      new UIGridConstraints(
+        1,
+        1,
+        1,
+        1,
+        UIGridConstraints.ANCHOR_WEST,
+        UIGridConstraints.FILL_NONE,
+        UIGridConstraints.SIZEPOLICY_CAN_SHRINK | UIGridConstraints.SIZEPOLICY_CAN_GROW,
+        UIGridConstraints.SIZEPOLICY_FIXED,
+        null,
+        null,
+        null,
+        0,
+        false
+      )
+    )
 
-    rootPanel.add(ignoreUnknownCACheckBox, new UIGridConstraints(
-      1,
-      1,
-      1,
-      1,
-      UIGridConstraints.ANCHOR_WEST,
-      UIGridConstraints.FILL_NONE,
-      UIGridConstraints.SIZEPOLICY_CAN_SHRINK | UIGridConstraints.SIZEPOLICY_CAN_GROW,
-      UIGridConstraints.SIZEPOLICY_FIXED,
-      null,
-      null,
-      null,
-      0,
-      false))
-
-    if (projectSettings.nonEmpty) {
-      val additionalParametersLabel = new JLabel
-      additionalParametersLabel.setText("Additional parameters:")
-
-      rootPanel.add(additionalParametersLabel, new UIGridConstraints(
-        3,
+    val organizationLabel = new JLabel("Organization:")
+    generalSettingsPanel.add(
+      organizationLabel,
+      new UIGridConstraints(
+        2,
         0,
         1,
         1,
@@ -165,22 +148,160 @@ class SettingsDialog(applicationSettings: SnykApplicationSettingsStateService,
         null,
         null,
         0,
-        false))
+        false
+      )
+    )
 
-      rootPanel.add(additionalParametersTextField, new UIGridConstraints(
-        3,
+    generalSettingsPanel.add(
+      organizationTextField,
+      new UIGridConstraints(
+        2,
         1,
         1,
         1,
         UIGridConstraints.ANCHOR_WEST,
-        UIGridConstraints.FILL_HORIZONTAL,
-        UIGridConstraints.SIZEPOLICY_WANT_GROW,
+        UIGridConstraints.SIZEPOLICY_FIXED,
+        UIGridConstraints.SIZEPOLICY_FIXED,
         UIGridConstraints.SIZEPOLICY_FIXED,
         null,
-        new Dimension(150, -1),
+        new Dimension(defaultTextFieldWidth, -1),
         null,
         0,
-        false))
+        false
+      )
+    )
+
+    val generalSettingsSpacer = new Spacer
+    generalSettingsPanel.add(
+      generalSettingsSpacer,
+      new UIGridConstraints(
+        3,
+        0,
+        1,
+        1,
+        UIGridConstraints.ANCHOR_CENTER,
+        UIGridConstraints.FILL_VERTICAL,
+        1,
+        UIGridConstraints.SIZEPOLICY_WANT_GROW,
+        null,
+        null,
+        null,
+        0,
+        false
+      )
+    )
+
+    customEndpointLabel.setLabelFor(customEndpointTextField)
+    organizationLabel.setLabelFor(organizationTextField)
+
+    if (projectSettings.nonEmpty) {
+      val projectSettingsPanel = new JPanel
+      projectSettingsPanel.setLayout(
+        new UIGridLayoutManager(2, 3, new Insets(0, 0, 0, 0), -1, -1))
+      projectSettingsPanel.setBorder(IdeBorderFactory.createTitledBorder("Project settings"))
+
+      rootPanel.add(
+        projectSettingsPanel,
+        new UIGridConstraints(
+          1,
+          0,
+          1,
+          1,
+          UIGridConstraints.ANCHOR_CENTER,
+          UIGridConstraints.FILL_BOTH,
+          UIGridConstraints.SIZEPOLICY_CAN_SHRINK | UIGridConstraints.SIZEPOLICY_CAN_GROW,
+          UIGridConstraints.SIZEPOLICY_CAN_SHRINK | UIGridConstraints.SIZEPOLICY_CAN_GROW,
+          null,
+          new Dimension(200, 200),
+          null,
+          0,
+          false
+        )
+      )
+
+      val additionalParametersLabel = new JLabel("Additional parameters:")
+      projectSettingsPanel.add(
+        additionalParametersLabel,
+        new UIGridConstraints(
+          0,
+          0,
+          1,
+          1,
+          UIGridConstraints.ANCHOR_WEST,
+          UIGridConstraints.FILL_NONE,
+          UIGridConstraints.SIZEPOLICY_FIXED,
+          UIGridConstraints.SIZEPOLICY_FIXED,
+          null,
+          new Dimension(120, 16),
+          null,
+          0,
+          false
+        )
+      )
+
+      projectSettingsPanel.add(
+        additionalParametersTextField,
+        new UIGridConstraints(
+          0,
+          1,
+          1,
+          1,
+          UIGridConstraints.ANCHOR_WEST,
+          UIGridConstraints.SIZEPOLICY_FIXED,
+          UIGridConstraints.SIZEPOLICY_FIXED,
+          UIGridConstraints.SIZEPOLICY_FIXED,
+          null,
+          new Dimension(defaultTextFieldWidth, -1),
+          null,
+          0,
+          false
+        )
+      )
+
+      additionalParametersLabel.setLabelFor(additionalParametersTextField)
+
+      val projectSettingsSpacer = new Spacer
+      projectSettingsPanel.add(
+        projectSettingsSpacer,
+        new UIGridConstraints(
+          1,
+          0,
+          1,
+          1,
+          UIGridConstraints.ANCHOR_CENTER,
+          UIGridConstraints.FILL_VERTICAL,
+          1,
+          UIGridConstraints.SIZEPOLICY_WANT_GROW,
+          null,
+          null,
+          null,
+          0,
+          false
+        )
+      )
+
+      val emptyPanel = new JPanel
+      emptyPanel.setLayout(
+        new UIGridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1))
+
+      rootPanel.add(
+        emptyPanel,
+        new UIGridConstraints(
+          2,
+          0,
+          1,
+          1,
+          UIGridConstraints.ANCHOR_CENTER,
+          UIGridConstraints.FILL_BOTH,
+          UIGridConstraints.SIZEPOLICY_CAN_SHRINK | UIGridConstraints.SIZEPOLICY_CAN_GROW,
+          UIGridConstraints.SIZEPOLICY_CAN_SHRINK | UIGridConstraints.SIZEPOLICY_CAN_GROW,
+          null,
+          new Dimension(200, 200),
+          null,
+          0,
+          false
+        )
+      )
     }
   }
 
