a64776c3ab29795a453422c8e3f5474044f428e4
Pavel Sorokin <60606414+pavel-snyk@users.noreply.github.com>
2020-09-02 01:30:01 +0200

    Use singleton instead of companion object for icons

--- src/main/kotlin/io/snyk/plugin/ui/Icons.kt
+++ src/main/kotlin/io/snyk/plugin/ui/Icons.kt
@@ -1,25 +1,20 @@
 package io.snyk.plugin.ui
 
 import com.intellij.openapi.util.IconLoader.getIcon
-import com.intellij.util.SVGLoader
 import javax.swing.ImageIcon
 
-class Icons {
-    companion object {
-        private fun getIconFromResources(name: String): ImageIcon = ImageIcon(this::class.java.getResource(name))
-        private fun getSvgIconFromResources(name: String): ImageIcon =
-            ImageIcon(SVGLoader.load(this::class.java.getResource(name), 1.0f))
+object Icons {
+    private fun getIconFromResources(name: String): ImageIcon = ImageIcon(this::class.java.getResource(name))
 
-        val VULNERABILITY_24 = getIconFromResources("/icons/vulnerability.png")
-        val VULNERABILITY_16 = getIconFromResources("/icons/vulnerability_16.png")
+    val VULNERABILITY_16 = getIconFromResources("/icons/vulnerability_16.png")
+    val VULNERABILITY_24 = getIconFromResources("/icons/vulnerability.png")
 
-        val HIGH_SEVERITY = getIcon("/icons/severity_high.svg")
-        val LOW_SEVERITY = getIcon("/icons/severity_low.svg")
-        val MEDIUM_SEVERITY = getIcon("/icons/severity_medium.svg")
+    val HIGH_SEVERITY = getIcon("/icons/severity_high.svg")
+    val LOW_SEVERITY = getIcon("/icons/severity_low.svg")
+    val MEDIUM_SEVERITY = getIcon("/icons/severity_medium.svg")
 
-        val GRADLE = getSvgIconFromResources("/icons/gradle.svg")
-        val MAVEN = getSvgIconFromResources("/icons/maven.svg")
-        val NPM = getSvgIconFromResources("/icons/npm.svg")
-        val PYTHON = getSvgIconFromResources("/icons/python.svg")
-    }
+    val GRADLE = getIcon("/icons/gradle.svg")
+    val MAVEN = getIcon("/icons/maven.svg")
+    val NPM = getIcon("/icons/npm.svg")
+    val PYTHON = getIcon("/icons/python.svg")
 }
