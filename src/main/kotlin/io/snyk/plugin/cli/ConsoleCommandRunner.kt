343c9190065d55f1d5f0e43f9cb57631de706bbf
Pavel Sorokin <60606414+pavel-snyk@users.noreply.github.com>
2020-09-14 01:43:09 +0200

    feat: set integration name and version values

--- src/main/kotlin/io/snyk/plugin/cli/ConsoleCommandRunner.kt
+++ src/main/kotlin/io/snyk/plugin/cli/ConsoleCommandRunner.kt
@@ -2,11 +2,18 @@ package io.snyk.plugin.cli
 
 import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.execution.process.ScriptRunnerUtil
+import com.intellij.ide.plugins.PluginManagerCore
+import io.snyk.plugin.SnykPostStartupActivity
 import org.apache.log4j.Logger
 import java.nio.charset.Charset
 
 open class ConsoleCommandRunner {
     private val logger: Logger = Logger.getLogger(ConsoleCommandRunner::class.java)
+
+    private val snykPluginVersion: String by lazy {
+        val featureTrainerPluginId = PluginManagerCore.getPluginByClassName(SnykPostStartupActivity::class.java.name)
+        PluginManagerCore.getPlugin(featureTrainerPluginId)?.version ?: "UNKNOWN"
+    }
 
     open fun execute(commands: List<String>, workDirectory: String = "/", apiToken: String = ""): String {
         logger.info("Enter ConsoleCommandRunner.execute()")
@@ -20,6 +27,8 @@ open class ConsoleCommandRunner {
         if (apiToken.isNotEmpty()) {
             generalCommandLine.environment["SNYK_TOKEN"] = apiToken
         }
+        generalCommandLine.environment["SNYK_INTEGRATION_NAME"] = "JETBRAINS_IDE"
+        generalCommandLine.environment["SNYK_INTEGRATION_VERSION"] = snykPluginVersion
 
         logger.info("GeneralCommandLine instance created.")
         logger.info("Execute ScriptRunnerUtil.getProcessOutput(...)")
