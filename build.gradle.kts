76dc1d948612ba555551cbc06ad913d861bb8f98
Pavel Sorokin <60606414+pavel-snyk@users.noreply.github.com>
2020-10-01 01:19:25 +0200

    fix: don't run patchChangelog task during publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,7 +72,6 @@ tasks {
   }
 
   publishPlugin {
-    dependsOn("patchChangelog")
     token(System.getenv("PUBLISH_TOKEN"))
     channels(pluginVersion.split('-').getOrElse(1) { "default" }.split('.').first())
   }
