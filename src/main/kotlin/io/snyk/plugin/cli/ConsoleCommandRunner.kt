1661caf802199d1fffa3f3c201eedf70620873a4
Aleksandr <aldanchenko@gmail.com>
2020-12-04 15:28:22 +0200

    feat: add JB type integration flag (#73)
    * feat: added integration flag for different JetBrains products.
    * feat: improve integration version name for IntelliJ Idea Ultimate and Community support, PyCharm Professional and Community support.

--- src/main/kotlin/io/snyk/plugin/cli/ConsoleCommandRunner.kt
+++ src/main/kotlin/io/snyk/plugin/cli/ConsoleCommandRunner.kt
@@ -3,6 +3,7 @@ package io.snyk.plugin.cli
 import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.execution.process.ScriptRunnerUtil
 import com.intellij.ide.plugins.PluginManagerCore
+import com.intellij.openapi.application.ApplicationInfo
 import io.snyk.plugin.SnykPostStartupActivity
 import org.apache.log4j.Logger
 import java.nio.charset.Charset
@@ -24,15 +25,33 @@ open class ConsoleCommandRunner {
         generalCommandLine.charset = Charset.forName("UTF-8")
         generalCommandLine.setWorkDirectory(workDirectory)
 
-        if (apiToken.isNotEmpty()) {
-            generalCommandLine.environment["SNYK_TOKEN"] = apiToken
-        }
-        generalCommandLine.environment["SNYK_INTEGRATION_NAME"] = "JETBRAINS_IDE"
-        generalCommandLine.environment["SNYK_INTEGRATION_VERSION"] = snykPluginVersion
+        setupCliEnvironmentVariables(generalCommandLine, apiToken)
 
         logger.info("GeneralCommandLine instance created.")
         logger.info("Execute ScriptRunnerUtil.getProcessOutput(...)")
 
         return ScriptRunnerUtil.getProcessOutput(generalCommandLine, ScriptRunnerUtil.STDOUT_OUTPUT_KEY_FILTER, 720000)
+    }
+
+    /**
+     * Setup environment variables for CLI.
+     */
+    fun setupCliEnvironmentVariables(generalCommandLine: GeneralCommandLine, apiToken: String) {
+        if (apiToken.isNotEmpty()) {
+            generalCommandLine.environment["SNYK_TOKEN"] = apiToken
+        }
+
+        generalCommandLine.environment["SNYK_INTEGRATION_NAME"] = "JETBRAINS_IDE"
+        generalCommandLine.environment["SNYK_INTEGRATION_VERSION"] = snykPluginVersion
+
+        val applicationInfo = ApplicationInfo.getInstance()
+
+        val versionName = when (val name = applicationInfo.versionName) {
+            "IntelliJ IDEA", "PyCharm" -> "$name ${applicationInfo.apiVersion.substring(0, 2)}"
+            else -> name
+        }
+
+        generalCommandLine.environment["SNYK_INTEGRATION_ENVIRONMENT"] = versionName.toUpperCase()
+        generalCommandLine.environment["SNYK_INTEGRATION_ENVIRONMENT_VERSION"] = applicationInfo.fullVersion
     }
 }
