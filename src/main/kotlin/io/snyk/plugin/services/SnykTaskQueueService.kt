c838065dc2638489d9fb06cee602af531c94c716
Pavel Sorokin <60606414+pavel-snyk@users.noreply.github.com>
2020-12-16 00:48:18 +0100

    Change scope to global for cli downloader service

--- src/main/kotlin/io/snyk/plugin/services/SnykTaskQueueService.kt
+++ src/main/kotlin/io/snyk/plugin/services/SnykTaskQueueService.kt
@@ -71,7 +71,7 @@ class SnykTaskQueueService(val project: Project) {
 
                 currentProgressIndicator = indicator
 
-                val cliDownloader = project.service<SnykCliDownloaderService>()
+                val cliDownloader = service<SnykCliDownloaderService>()
 
                 indicator.checkCanceled()
 
