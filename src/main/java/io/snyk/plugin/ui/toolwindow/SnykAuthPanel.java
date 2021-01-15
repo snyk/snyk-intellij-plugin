0a537c593b8ba889a5c5d884f93e7afa09d15fa8
Artsiom Chapialiou <achapialiou@gmail.com>
2021-01-15 15:25:38 +0300

    invoke auth action right from Auth panel.

--- src/main/java/io/snyk/plugin/ui/toolwindow/SnykAuthPanel.java
+++ src/main/java/io/snyk/plugin/ui/toolwindow/SnykAuthPanel.java
@@ -1,12 +1,16 @@
 package io.snyk.plugin.ui.toolwindow;
 
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.options.ShowSettingsUtil;
 import com.intellij.openapi.project.Project;
 import io.snyk.plugin.events.SnykCliDownloadListener;
+import io.snyk.plugin.services.SnykCliAuthenticationService;
 import io.snyk.plugin.settings.SnykProjectSettingsConfigurable;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+
+import static io.snyk.plugin.UtilsKt.getApplicationSettingsStateService;
 
 public class SnykAuthPanel {
   private JPanel rootPanel;
@@ -16,7 +20,10 @@ public class SnykAuthPanel {
     connectIntelliJToSnykButton.addActionListener(e -> {
       // todo
       project.getService(SnykToolWindowPanel.class).cleanAll();
-      ShowSettingsUtil.getInstance().showSettingsDialog(project, SnykProjectSettingsConfigurable.class);
+//      ShowSettingsUtil.getInstance().showSettingsDialog(project, SnykProjectSettingsConfigurable.class);
+      getApplicationSettingsStateService().setToken(
+        ApplicationManager.getApplication().getService(SnykCliAuthenticationService.class).authenticate()
+      );
       project.getMessageBus().syncPublisher(SnykCliDownloadListener.Companion.getCLI_DOWNLOAD_TOPIC())
         .checkCliExistsFinished();
     });
