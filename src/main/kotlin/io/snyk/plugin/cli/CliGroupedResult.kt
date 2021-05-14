5750099bcdec1129785a16e4d63a229b1d801af3
Artsiom Chapialiou <achapialiou@gmail.com>
2021-05-14 22:10:06 +0300

    fix: json with missed fields parsing
    Null value for possible not existing fields gotten from JSON (license vulnerability).

--- src/main/kotlin/io/snyk/plugin/cli/CliGroupedResult.kt
+++ src/main/kotlin/io/snyk/plugin/cli/CliGroupedResult.kt
@@ -1,9 +1,7 @@
 package io.snyk.plugin.cli
 
-class CliGroupedResult(
+data class CliGroupedResult(
     val id2vulnerabilities: Map<String, List<Vulnerability>>,
     val uniqueCount: Int,
-    val pathsCount: Int,
-    val projectName: String,
-    val displayTargetFile: String,
-    val path: String)
+    val pathsCount: Int
+)
--- src/main/kotlin/io/snyk/plugin/cli/CliVulnerabilitiesForFile.kt
+++ src/main/kotlin/io/snyk/plugin/cli/CliVulnerabilitiesForFile.kt
@@ -4,17 +4,15 @@ class CliVulnerabilitiesForFile {
     lateinit var vulnerabilities: Array<Vulnerability>
     lateinit var projectName: String
     lateinit var displayTargetFile: String
-    lateinit var path: String
     lateinit var packageManager: String
 
     val uniqueCount: Int get() = vulnerabilities.groupBy { it.id }.size
-    var foundProjectCount: Int = 0
 
     fun toCliGroupedResult(): CliGroupedResult {
         val id2vulnerabilities = vulnerabilities.groupBy({ it.id }, { it })
         val uniqueCount = id2vulnerabilities.keys.size
         val pathsCount = id2vulnerabilities.values.flatten().size
 
-        return CliGroupedResult(id2vulnerabilities, uniqueCount, pathsCount, projectName, displayTargetFile, path)
+        return CliGroupedResult(id2vulnerabilities, uniqueCount, pathsCount)
     }
 }
