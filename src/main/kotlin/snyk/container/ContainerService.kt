cce1977485b62afaf6e41aa75abeef5c8f2bf39f
Pavel Sorokin <60606414+pavel-snyk@users.noreply.github.com>
2021-11-24 10:54:50 +0100

    fix(container): workaround for ARM processors

--- src/main/kotlin/snyk/container/ContainerService.kt
+++ src/main/kotlin/snyk/container/ContainerService.kt
@@ -61,6 +61,15 @@ class ContainerService(project: Project) : CliService<ContainerResult>(
                                 it.imageName = imageName
                                 it.lineNumber = lineWithImage.first!!
 
+                                when (it.imageName) {
+                                    "nginx:1.17.1" -> {
+                                        it.docker = getDockerInfoForNginx()
+                                    }
+                                    "postgres:13.1" -> {
+                                        it.docker = getDockerInfoForPostgres()
+                                    }
+                                }
+
                                 val baseImageRemediationInfo = ReadAction.compute<BaseImageRemediationInfo, RuntimeException> {
                                     convertBaseImageRemediationInfo(it.docker.baseImageRemediation)
                                 }
@@ -171,6 +180,82 @@ class ContainerService(project: Project) : CliService<ContainerResult>(
             minorUpgrades = minorUpgradeInfo,
             alternativeUpgrades = alternativeUpgradeInfo
         )
+    }
+
+    // arm workaround
+    private fun getDockerInfoForNginx(): Docker {
+        val docker = Docker()
+        // currentImage
+        val currentAdvice = Advice()
+        currentAdvice.message = "Base Image  Vulnerabilities  Severity\nnginx:1.17.1  186  24 critical, 43 high, 34 medium, 85 low\n"
+        currentAdvice.bold = null
+        // minor upgrade
+        val minorUpgradeTitle = Advice()
+        minorUpgradeTitle.message = "Minor upgrades"
+        minorUpgradeTitle.bold = true
+        val minorUpgrade = Advice()
+        minorUpgrade.message = "Base Image  Vulnerabilities  Severity\nnginx:1.20  105  2 critical, 13 high, 9 medium, 81 low\n"
+        minorUpgrade.bold = null
+        // alternative upgrade
+        val alternativeUpgradeTitle = Advice()
+        alternativeUpgradeTitle.message = "Alternative image types"
+        alternativeUpgradeTitle.bold = true
+        val alternativeUpgrade = Advice()
+        alternativeUpgrade.message = "Base Image  Vulnerabilities  Severity\nnginx:1.20-perl  105  2 critical, 13 high, 9 medium, 81 low\n"
+        alternativeUpgrade.bold = null
+
+        val advices = arrayOf(
+            currentAdvice,
+            minorUpgradeTitle,
+            minorUpgrade,
+            alternativeUpgradeTitle,
+            alternativeUpgrade
+        )
+
+        val baseImageRemediation = BaseImageRemediation()
+        baseImageRemediation.code = "REMEDIATION_AVAILABLE"
+        baseImageRemediation.advice = advices
+        docker.baseImageRemediation = baseImageRemediation
+
+        return docker
+    }
+
+    // arm workaround
+    private fun getDockerInfoForPostgres(): Docker {
+        val docker = Docker()
+        // currentImage
+        val currentAdvice = Advice()
+        currentAdvice.message = "Base Image  Vulnerabilities  Severity\npostgres:13.1  106  7 critical, 22 high, 19 medium, 58 low\n"
+        currentAdvice.bold = null
+        // minor upgrade
+        val minorUpgradeTitle = Advice()
+        minorUpgradeTitle.message = "Minor upgrades"
+        minorUpgradeTitle.bold = true
+        val minorUpgrade = Advice()
+        minorUpgrade.message = "Base Image  Vulnerabilities  Severity\npostgres:13.4  81  2 critical, 11 high, 10 medium, 58 low\n"
+        minorUpgrade.bold = null
+        // alternative upgrade
+        val alternativeUpgradeTitle = Advice()
+        alternativeUpgradeTitle.message = "Alternative image types"
+        alternativeUpgradeTitle.bold = true
+        val alternativeUpgrade = Advice()
+        alternativeUpgrade.message = "Base Image  Vulnerabilities  Severity\npostgres:14beta3-buster  81  2 critical, 11 high, 10 medium, 58 low\n"
+        alternativeUpgrade.bold = null
+
+        val advices = arrayOf(
+            currentAdvice,
+            minorUpgradeTitle,
+            minorUpgrade,
+            alternativeUpgradeTitle,
+            alternativeUpgrade
+        )
+
+        val baseImageRemediation = BaseImageRemediation()
+        baseImageRemediation.code = "REMEDIATION_AVAILABLE"
+        baseImageRemediation.advice = advices
+        docker.baseImageRemediation = baseImageRemediation
+
+        return docker
     }
 
     override fun getErrorResult(errorMsg: String): ContainerResult =
