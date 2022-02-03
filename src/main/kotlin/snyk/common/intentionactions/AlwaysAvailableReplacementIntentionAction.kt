303d97a0b8b9770baee7d3de432b03d57620c930
Bastian Doetsch <bastian.doetsch@snyk.io>
2022-02-03 11:50:31 +0100

    fix: 🐛 save document after applying action intention (quickfix) [ROAD-667]
    - updates caches
    - triggers recalculation of annotations (hopefully)

--- src/main/kotlin/snyk/common/intentionactions/AlwaysAvailableReplacementIntentionAction.kt
+++ src/main/kotlin/snyk/common/intentionactions/AlwaysAvailableReplacementIntentionAction.kt
@@ -4,6 +4,7 @@ import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
 import com.intellij.codeInsight.intention.IntentionAction
 import com.intellij.openapi.application.WriteAction
 import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiFile
@@ -36,7 +37,9 @@ class AlwaysAvailableReplacementIntentionAction(
         WriteAction.run<RuntimeException> {
             doc.replaceString(range.startOffset, range.endOffset, replacementText)
             FileContentUtil.reparseOpenedFiles()
-            DaemonCodeAnalyzer.getInstance(project).restart()
+            // save all changes on disk to update caches through SnykBulkFileListener
+            FileDocumentManager.getInstance().saveDocument(doc)
+            DaemonCodeAnalyzer.getInstance(project).restart(file)
         }
     }
 
