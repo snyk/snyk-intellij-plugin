eb5f49ba963dc3031ca689af58351b2db876dd37
Bastian Doetsch <bastian.doetsch@snyk.io>
2023-04-17 11:45:58 +0530

    fix: only add authentication to okhttpclient if configured

--- src/main/kotlin/io/snyk/plugin/net/RetrofitAuthenticator.kt
+++ src/main/kotlin/io/snyk/plugin/net/RetrofitAuthenticator.kt
@@ -10,16 +10,23 @@ import okhttp3.Route
 const val PROXY_AUTHORIZATION_HEADER_NAME = "Proxy-Authorization"
 
 class RetrofitAuthenticator(
+    // injectable httpconfigurable for testing
     private var httpConfigurable: HttpConfigurable? = null
 ) : Authenticator {
     override fun authenticate(route: Route?, response: Response): Request {
         if (httpConfigurable == null) {
             httpConfigurable = HttpConfigurable.getInstance()
         }
-        val prompt = "Snyk: Please enter your proxy credentials for connecting"
-        val auth = httpConfigurable!!.getPromptedAuthentication(response.request.url.host, prompt)
 
-        if (auth.userName == null || auth.password == null) {
+        // if no proxy configuration available, we just return the request
+        if (httpConfigurable?.PROXY_AUTHENTICATION == false) {
+            return response.request
+        }
+
+        val prompt = "Snyk: Please enter your proxy credentials for connecting"
+        val auth = httpConfigurable?.getPromptedAuthentication(response.request.url.host, prompt)
+
+        if (auth == null || auth.userName.isNullOrBlank() || auth.password == null || auth.password.isEmpty()) {
             return response.request
         }
 
