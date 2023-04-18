f0dc6d784af46f7f3d8ef8b1ad3e120e90c8151c
michelkaporin <michel@snyk.io>
2023-04-18 12:26:20 +0200

    test: No authorization header added for non-authenticated proxy

--- src/test/kotlin/io/snyk/plugin/net/RetrofitAuthenticatorTest.kt
+++ src/test/kotlin/io/snyk/plugin/net/RetrofitAuthenticatorTest.kt
@@ -18,6 +18,7 @@ class RetrofitAuthenticatorTest {
         val httpConfigurableMock = mockk<HttpConfigurable>()
         val auth = mockk<PasswordAuthentication>()
         every { httpConfigurableMock.getPromptedAuthentication(any(), any()) } returns auth
+        httpConfigurableMock.PROXY_AUTHENTICATION = true
         every { auth.userName } returns "username"
         every { auth.password } returns "pw".toCharArray()
         val cut = RetrofitAuthenticator(httpConfigurableMock)
@@ -36,5 +37,25 @@ class RetrofitAuthenticatorTest {
         assertNotNull(authHeader)
         assertEquals("Basic dXNlcm5hbWU6cHc=", authHeader)
         assertEquals("dXNlcm5hbWU6cHc=", Base64.encode("username:pw".toByteArray()))
+    }
+
+    @Test
+    fun `No authorization header added for non-authenticated proxy`() {
+        val httpConfigurableMock = mockk<HttpConfigurable>()
+        httpConfigurableMock.PROXY_AUTHENTICATION = false
+        val cut = RetrofitAuthenticator(httpConfigurableMock)
+
+        val route: Route = mockk()
+        val response: Response = Response.Builder()
+            .request(okhttp3.Request.Builder().url("https://example.com").build())
+            .code(200)
+            .protocol(okhttp3.Protocol.HTTP_1_1)
+            .message("")
+            .build()
+
+        val request = cut.authenticate(route, response)
+
+        val authHeader = request!!.headers[PROXY_AUTHORIZATION_HEADER_NAME]
+        assertEquals(null, authHeader)
     }
 }
