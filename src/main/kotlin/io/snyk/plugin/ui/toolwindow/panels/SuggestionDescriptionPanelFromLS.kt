458cea89b44626134cc5539016580592ae7876c0
Teodora Sandu <teodora.sandu@snyk.io>
2024-03-28 16:13:27 +0000

    test: try to fix tests

--- src/main/kotlin/io/snyk/plugin/ui/toolwindow/panels/SuggestionDescriptionPanelFromLS.kt
+++ src/main/kotlin/io/snyk/plugin/ui/toolwindow/panels/SuggestionDescriptionPanelFromLS.kt
@@ -4,8 +4,6 @@ import com.intellij.icons.AllIcons
 import com.intellij.ui.HyperlinkLabel
 import com.intellij.ui.ScrollPaneFactory
 import com.intellij.ui.components.JBTabbedPane
-import com.intellij.ui.jcef.JBCefApp
-import com.intellij.ui.jcef.JBCefBrowserBuilder
 import com.intellij.uiDesigner.core.GridConstraints
 import com.intellij.uiDesigner.core.GridLayoutManager
 import com.intellij.util.ui.JBInsets
@@ -19,6 +17,7 @@ import io.snyk.plugin.ui.DescriptionHeaderPanel
 import io.snyk.plugin.ui.SnykBalloonNotificationHelper
 import io.snyk.plugin.ui.baseGridConstraintsAnchorWest
 import io.snyk.plugin.ui.descriptionHeaderPanel
+import io.snyk.plugin.ui.getJBCefBrowserIfSupported
 import io.snyk.plugin.ui.panelGridConstraints
 import io.snyk.plugin.ui.toolwindow.SnykToolWindowPanel
 import io.snyk.plugin.ui.wrapWithScrollPane
@@ -49,14 +48,12 @@ class SuggestionDescriptionPanelFromLS(
 
     init {
         if (pluginSettings().isGlobalIgnoresFeatureEnabled && issue.additionalData.details != null) {
-            if (!JBCefApp.isSupported()) {
+            val (jbCefBrowser, jbCefBrowserUrl)  = getJBCefBrowserIfSupported()
+            if (jbCefBrowser == null) {
                 val statePanel = StatePanel(SnykToolWindowPanel.SELECT_ISSUE_TEXT)
                 this.add(wrapWithScrollPane(statePanel), BorderLayout.CENTER)
                 SnykBalloonNotificationHelper.showError(unexpectedErrorMessage, null)
             } else {
-                val cefClient = JBCefApp.getInstance().createClient()
-                val jbCefBrowser = JBCefBrowserBuilder().setClient(cefClient).setEnableOpenDevToolsMenuItem(true).build()
-
                 val panel = JPanel()
                 panel.add(jbCefBrowser.component, BorderLayout())
                 this.add(
@@ -64,10 +61,9 @@ class SuggestionDescriptionPanelFromLS(
                     BorderLayout.CENTER
                 )
 
-                jbCefBrowser.loadHTML(issue.additionalData.details, jbCefBrowser.cefBrowser.url)
+                jbCefBrowser.loadHTML(issue.additionalData.details, jbCefBrowserUrl)
             }
         } else {
-
             createUI()
         }
     }
