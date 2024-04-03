7b012b92c912dba5fe477b4ec796c75a977b271b
Bastian Doetsch <bastian.doetsch@snyk.io>
2024-04-03 11:07:05 +0200

    fix: substitue link labels and insets with action link and jbinsets

--- src/main/kotlin/io/snyk/plugin/ui/toolwindow/panels/StatePanel.kt
+++ src/main/kotlin/io/snyk/plugin/ui/toolwindow/panels/StatePanel.kt
@@ -1,16 +1,17 @@
 package io.snyk.plugin.ui.toolwindow.panels
 
-import com.intellij.ui.components.labels.LinkLabel
+import com.intellij.ui.components.ActionLink
 import com.intellij.uiDesigner.core.GridLayoutManager
+import com.intellij.util.ui.JBUI
 import io.snyk.plugin.ui.addSpacer
 import io.snyk.plugin.ui.baseGridConstraints
 import io.snyk.plugin.ui.getReadOnlyClickableHtmlJEditorPaneFixedSize
-import java.awt.Insets
+import java.awt.event.ActionEvent
 import javax.swing.JPanel
 
-class StatePanel(messageHtmlText: String, actionText: String? = null, action: (() -> Unit)? = null) : JPanel() {
+class StatePanel(messageHtmlText: String, actionText: String? = null, action: ((e: ActionEvent) -> Unit)? = null) : JPanel() {
     init {
-        layout = GridLayoutManager(4, 1, Insets(20, 100, 20, 100), -1, -1)
+        layout = GridLayoutManager(4, 1, JBUI.insets(20, 100), -1, -1)
 
         add(
             getReadOnlyClickableHtmlJEditorPaneFixedSize(messageHtmlText),
@@ -19,7 +20,7 @@ class StatePanel(messageHtmlText: String, actionText: String? = null, action: ((
 
         if (actionText != null && action != null) {
             add(
-                LinkLabel.create(actionText, action),
+                ActionLink(actionText, action),
                 baseGridConstraints(row = 2, indent = 0)
             )
         }
