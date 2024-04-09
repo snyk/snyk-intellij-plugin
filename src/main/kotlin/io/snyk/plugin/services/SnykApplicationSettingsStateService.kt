419048cc051e2b623201ec9560ebc908cf3f29f4
Bastian Doetsch <bastian.doetsch@snyk.io>
2024-04-09 12:36:22 +0200

    fix: merge error

--- src/main/kotlin/io/snyk/plugin/services/SnykApplicationSettingsStateService.kt
+++ src/main/kotlin/io/snyk/plugin/services/SnykApplicationSettingsStateService.kt
@@ -27,7 +27,6 @@ import java.util.UUID
 )
 class SnykApplicationSettingsStateService : PersistentStateComponent<SnykApplicationSettingsStateService> {
 
-    var currentLSProtocolVersion: Int? = 0
     val requiredLsProtocolVersion = 11
 
     var currentLSProtocolVersion: Int? = 0
