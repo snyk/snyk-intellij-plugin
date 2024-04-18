0460fe45833551664ce07743232ebea6b965be68
Teodora Sandu <81559517+teodora-sandu@users.noreply.github.com>
2024-04-18 10:38:10 +0100

    fix: settings for scan types (#511)
    * fix: settings for scan types
    
    * fix: settings for severity types
    
    * fix: settings for issue option types
    
    * refactor: improve code readability

--- src/main/kotlin/io/snyk/plugin/ui/settings/IssueViewOptionsPanel.kt
+++ src/main/kotlin/io/snyk/plugin/ui/settings/IssueViewOptionsPanel.kt
@@ -2,6 +2,8 @@ package io.snyk.plugin.ui.settings
 
 import com.intellij.openapi.project.Project
 import com.intellij.ui.components.JBCheckBox
+import com.intellij.ui.dsl.builder.actionListener
+import com.intellij.ui.dsl.builder.bindSelected
 import com.intellij.util.ui.JBUI
 import io.snyk.plugin.getSnykTaskQueueService
 import io.snyk.plugin.pluginSettings
@@ -13,52 +15,57 @@ class IssueViewOptionsPanel(
     private val settings
         get() = pluginSettings()
 
+    private var currentOpenIssuesEnabled = settings.openIssuesEnabled
+    private var currentIgnoredIssuesEnabled = settings.ignoredIssuesEnabled
+
     val panel = com.intellij.ui.dsl.builder.panel {
         row {
             checkBox(
                 text = "Open issues",
-            ).component.apply {
-                isSelected = settings.openIssuesEnabled
-                this.addItemListener {
-                    if (canOptionChange(this, settings.openIssuesEnabled)) {
-                        settings.openIssuesEnabled = this.isSelected
-                        getSnykTaskQueueService(project)?.scan()
-                    }
-                }
-                name = "Open issues"
+            ).applyToComponent {
+                name = text
             }
+            .actionListener{ event, it ->
+                if (canBeChanged(it, it.isSelected)) {
+                    currentOpenIssuesEnabled = it.isSelected
+                    getSnykTaskQueueService(project)?.scan()
+                }
+            }
+            // bindSelected is needed to trigger apply() on the settings dialog that this panel is rendered in
+            // that way we trigger the re-rendering of the Tree Nodes
+            .bindSelected(settings::openIssuesEnabled)
         }
         row {
                 checkBox(
                     text = "Ignored issues",
-                ).component.apply {
-                    isSelected = settings.ignoredIssuesEnabled
-                    this.addItemListener {
-                        if (canOptionChange(this, settings.ignoredIssuesEnabled)) {
-                            settings.ignoredIssuesEnabled = this.isSelected
-                            getSnykTaskQueueService(project)?.scan()
-                        }
-                    }
-                    name = "Ignored issues"
+                ).applyToComponent {
+                    name = text
                 }
+                .actionListener{ event, it ->
+                    if (canBeChanged(it, it.isSelected)) {
+                        currentIgnoredIssuesEnabled = it.isSelected
+                        getSnykTaskQueueService(project)?.scan()
+                    }
+                }
+                .bindSelected(settings::ignoredIssuesEnabled)
         }
     }.apply {
         name = "issueViewPanel"
         border = JBUI.Borders.empty(2)
     }
 
-    private fun canOptionChange(component: JBCheckBox, wasEnabled: Boolean): Boolean {
+    private fun canBeChanged(component: JBCheckBox, isSelected: Boolean): Boolean {
         val onlyOneEnabled = arrayOf(
-            settings.openIssuesEnabled,
-            settings.ignoredIssuesEnabled,
+            currentOpenIssuesEnabled,
+            currentIgnoredIssuesEnabled,
         ).count { it } == 1
 
-        if (onlyOneEnabled && wasEnabled) {
-            component.isSelected = true
+        if (onlyOneEnabled && !isSelected) {
             SnykBalloonNotificationHelper.showWarnBalloonForComponent(
                 "At least one option should be selected",
                 component
             )
+            component.isSelected = true
             return false
         }
         return true
--- src/main/kotlin/io/snyk/plugin/ui/settings/SeveritiesEnablementPanel.kt
+++ src/main/kotlin/io/snyk/plugin/ui/settings/SeveritiesEnablementPanel.kt
@@ -1,12 +1,13 @@
 package io.snyk.plugin.ui.settings
 
 import com.intellij.ui.components.JBCheckBox
+import com.intellij.ui.dsl.builder.actionListener
+import com.intellij.ui.dsl.builder.bindSelected
 import com.intellij.ui.dsl.builder.panel
 import com.intellij.util.ui.JBUI
 import io.snyk.plugin.Severity
 import io.snyk.plugin.pluginSettings
 import io.snyk.plugin.ui.SnykBalloonNotificationHelper
-import java.awt.event.ItemEvent
 
 class SeveritiesEnablementPanel {
     private val settings
@@ -21,54 +22,60 @@ class SeveritiesEnablementPanel {
         row {
             checkBox(Severity.CRITICAL.toPresentableString()).applyToComponent {
                 name = text
-                isSelected = settings.criticalSeverityEnabled
-                this.addItemListener {
-                    correctLastSeverityDisabled(it)
-                    settings.criticalSeverityEnabled = this.isSelected
+            }
+            .actionListener{ event, it ->
+                val isSelected = it.isSelected
+                if (canBeChanged(it, isSelected)) {
+                    // we need to change the settings in here in order for the validation to work pre-apply
+                    currentCriticalSeverityEnabled = isSelected
                 }
             }
+            // bindSelected is needed to trigger apply() on the settings dialog that this panel is rendered in
+            // that way we trigger the re-rendering of the Tree Nodes
+            .bindSelected(settings::criticalSeverityEnabled)
         }
         row {
             checkBox(Severity.HIGH.toPresentableString()).applyToComponent {
                 name = text
-                isSelected = settings.highSeverityEnabled
-                this.addItemListener {
-                    correctLastSeverityDisabled(it)
-                    settings.highSeverityEnabled = this.isSelected
+            }
+            .actionListener{ event, it ->
+                val isSelected = it.isSelected
+                if(canBeChanged(it, isSelected)) {
+                    currentHighSeverityEnabled = isSelected
                 }
             }
+            .bindSelected(settings::highSeverityEnabled)
         }
         row {
             checkBox(Severity.MEDIUM.toPresentableString()).applyToComponent {
                 name = text
-                isSelected = settings.mediumSeverityEnabled
-                this.addItemListener {
-                    correctLastSeverityDisabled(it)
-                    settings.mediumSeverityEnabled = this.isSelected
+            }
+            .actionListener{ event, it ->
+                val isSelected = it.isSelected
+                if (canBeChanged(it, isSelected)) {
+                    currentMediumSeverityEnabled = isSelected
                 }
             }
+            .bindSelected(settings::mediumSeverityEnabled)
         }
         row {
             checkBox(Severity.LOW.toPresentableString()).applyToComponent {
                 name = text
-                isSelected = settings.lowSeverityEnabled
-                this.addItemListener {
-                    correctLastSeverityDisabled(it)
-                    settings.lowSeverityEnabled = this.isSelected
+            }
+            .actionListener{ event, it ->
+                val isSelected = it.isSelected
+                if (canBeChanged(it, isSelected)) {
+                    currentLowSeverityEnabled = isSelected
                 }
             }
+            .bindSelected(settings::lowSeverityEnabled)
         }
     }.apply {
         name = "severityEnablementPanel"
         border = JBUI.Borders.empty(2)
     }
 
-    private fun JBCheckBox.correctLastSeverityDisabled(it: ItemEvent) {
-        val deselected = it.stateChange == ItemEvent.DESELECTED
-        isLastSeverityDisabling(this, deselected)
-    }
-
-    private fun isLastSeverityDisabling(component: JBCheckBox, wasEnabled: Boolean): Boolean {
+    private fun canBeChanged(component: JBCheckBox, isSelected: Boolean): Boolean {
         val onlyOneEnabled = arrayOf(
             currentCriticalSeverityEnabled,
             currentHighSeverityEnabled,
@@ -76,13 +83,14 @@ class SeveritiesEnablementPanel {
             currentLowSeverityEnabled
         ).count { it } == 1
 
-        if (onlyOneEnabled && wasEnabled) {
-            component.isSelected = true
+        if (onlyOneEnabled && !isSelected) {
             SnykBalloonNotificationHelper.showWarnBalloonForComponent(
                 "At least one Severity type should be selected",
                 component
             )
+            component.isSelected = true
+            return false
         }
-        return onlyOneEnabled
+        return true
     }
 }
