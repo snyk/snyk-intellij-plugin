5b72423463af1bd7f4a1db349226c30ee0cf2e5f
Bastian Doetsch <bastian.doetsch@snyk.io>
2024-09-14 10:11:06 +0200

    fix: early exit editor painting if no issues known

--- src/main/kotlin/snyk/common/editor/LineEndingEditorFactoryListener.kt
+++ src/main/kotlin/snyk/common/editor/LineEndingEditorFactoryListener.kt
@@ -17,6 +17,7 @@ import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiCompiledElement
 import com.intellij.psi.PsiDocumentManager
+import io.snyk.plugin.getSnykCachedResults
 import io.snyk.plugin.toLanguageServerURL
 import io.snyk.plugin.ui.toolwindow.SnykPluginDisposable
 import org.eclipse.lsp4j.InlineValueContext
@@ -69,15 +70,12 @@ class LineEndingEditorFactoryListener : EditorFactoryListener, Disposable {
         editor.registerLineExtensionPainter(EditorLineEndingProvider(editor, psiFile.virtualFile)::getLineExtension)
     }
 
-    class EditorLineEndingProvider(val editor: Editor, val virtualFile: VirtualFile) : Disposable {
-        private val attributeKey: TextAttributesKey = DefaultLanguageHighlighterColors.LINE_COMMENT
-        val attributes: TextAttributes = EditorColorsManager.getInstance().globalScheme.getAttributes(attributeKey)
 
-        val logger = Logger.getInstance(this::class.java).apply {
-            // tie log level to language server log level
-            val languageServerWrapper = LanguageServerWrapper.getInstance()
-            if (languageServerWrapper.logger.isDebugEnabled) this.setLevel(LogLevel.DEBUG)
-            if (languageServerWrapper.logger.isTraceEnabled) this.setLevel(LogLevel.TRACE)
+    class EditorLineEndingProvider(val editor: Editor, val virtualFile: VirtualFile) : Disposable {
+        companion object {
+            private val attributeKey: TextAttributesKey = DefaultLanguageHighlighterColors.LINE_COMMENT
+            val attributes: TextAttributes = EditorColorsManager.getInstance().globalScheme.getAttributes(attributeKey)
+            val ctx = InlineValueContext(0, Range())
         }
 
         private var disposed = false
@@ -86,6 +84,7 @@ class LineEndingEditorFactoryListener : EditorFactoryListener, Disposable {
             }
 
         fun isDisposed() = disposed
+
         override fun dispose() {
             disposed = true
         }
@@ -94,25 +93,36 @@ class LineEndingEditorFactoryListener : EditorFactoryListener, Disposable {
             Disposer.register(SnykPluginDisposable.getInstance(), this)
         }
 
+        val project = editor.project
+        val document = editor.document
+        val snykCachedResults = project?.let { getSnykCachedResults(it) }
+        val languageServerWrapper = LanguageServerWrapper.getInstance()
+        val logger = Logger.getInstance(this::class.java).apply {
+            // tie log level to language server log level
+            if (languageServerWrapper.logger.isDebugEnabled) this.setLevel(LogLevel.DEBUG)
+            if (languageServerWrapper.logger.isTraceEnabled) this.setLevel(LogLevel.TRACE)
+        }
+
         fun getLineExtension(line: Int): MutableCollection<out LineExtensionInfo> {
-            if (disposed || !LanguageServerWrapper.getInstance()
-                    .ensureLanguageServerInitialized()
+            // only OSS has inline values right now
+            val hasResults = snykCachedResults?.currentOSSResultsLS?.isNotEmpty() ?: false
+            if (disposed
+                || !languageServerWrapper.isInitialized
+                || !hasResults
             ) return mutableSetOf()
 
-            val document = editor.document
             val lineEndOffset = document.getLineEndOffset(line)
             val lineStartOffset = document.getLineStartOffset(line)
             val range = Range(Position(line, 0), Position(line, lineEndOffset - lineStartOffset))
-            val ctx = InlineValueContext(0, range)
             val params = InlineValueParams(TextDocumentIdentifier(virtualFile.toLanguageServerURL()), range, ctx)
-
-            val inlineValue = LanguageServerWrapper.getInstance().languageServer.textDocumentService.inlineValue(params)
-            @Suppress("USELESS_CAST") val text = try {
-                val result = inlineValue.get(120, TimeUnit.SECONDS)
+            val inlineValue = languageServerWrapper.languageServer.textDocumentService.inlineValue(params)
+            val text = try {
+                val result = inlineValue.get(5, TimeUnit.MILLISECONDS)
                 if (result != null && result.isNotEmpty()) {
                     // this supposedly unneeded cast is needed as the conversion in lsp4j does not work correctly
-                    val firstInlineValue = (result as List<Either<InlineValueText, *>>)[0]
-                    firstInlineValue.left.text
+                    val eitherList = result as List<Either<InlineValueText, *>>
+                    val firstInlineValue = eitherList[0]
+                    firstInlineValue.left.text ?: ""
                 } else {
                     ""
                 }
