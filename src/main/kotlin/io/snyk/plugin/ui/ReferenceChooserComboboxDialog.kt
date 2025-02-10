09856e4c1f43d1e14fdfd98744621b85f82e40cb
Abdelrahman Shawki Hassan <shawki.hassan@snyk.io>
2025-02-10 13:37:32 +0100

    fix: revert change in ReferenceChooserComboboxDialog.kt

--- src/main/kotlin/io/snyk/plugin/ui/ReferenceChooserComboboxDialog.kt
+++ src/main/kotlin/io/snyk/plugin/ui/ReferenceChooserComboboxDialog.kt
@@ -10,7 +10,7 @@ import com.intellij.openapi.ui.TextFieldWithBrowseButton
 import com.intellij.openapi.ui.ValidationInfo
 import com.intellij.util.ui.GridBag
 import com.intellij.util.ui.JBUI
-import org.jetbrains.concurrency.runAsync
+import io.snyk.plugin.runInBackground
 import snyk.common.lsp.FolderConfig
 import snyk.common.lsp.LanguageServerWrapper
 import snyk.common.lsp.settings.FolderConfigSettings
@@ -103,7 +103,7 @@ class ReferenceChooserDialog(val project: Project) : DialogWrapper(true) {
         }
 
         if (doValidate() == null) {
-            runAsync {
+            runInBackground("Snyk: updating configuration") {
                 LanguageServerWrapper.getInstance().updateConfiguration(true)
             }
         }
