93536ebc7d95d4d37530cfb96e6f4f496a61dbad
Ben Durrans <Benjamin.Durrans@snyk.io>
2025-05-14 15:46:14 +0100

    fix: TextEdits applying to the wrong lines (#697)
    The previous code was based on bad logic for creating WorkspaceEdits in the LS.
    This LS bug has been fixed, but it isn't compatible with our code which expected the incorrect WorkspaceEdits.
    This has been fixed by applying the edits from the end of the file to the start of the file.

--- src/main/kotlin/snyk/common/editor/DocumentChanger.kt
+++ src/main/kotlin/snyk/common/editor/DocumentChanger.kt
@@ -10,7 +10,13 @@ object DocumentChanger {
         val fileURI = change.key
         val virtualFile = VirtualFileManager.getInstance().findFileByUrl(fileURI) ?: return
         val document = virtualFile.getDocument() ?: return
-        for (e in change.value) {
+        // Our LS is coded to give us the TextEdits in ascending order, but we must apply them in descending order.
+        // Imagine we had an edit that added 10 lines to the start of the file followed by an edit that added a line to the 4th line of the file,
+        // ascending order applying would incorrectly place the second edit withing the 10 lines added at the start!
+        // N.b. Line edits on a single line are returned as two edits, a deletion of the line and an addition to the line after, so reversing works for this too.
+        // N.b. The previous note is a lie for edits at the end of the file when no LF at EOF is present, for those the addition is marked for the same line as the deletion.
+        // TODO - Handle edits at EOF when there is no LF at EOF.
+        for (e in change.value.reversed()) {
             // normalize range
             var startLine = e.range.start.line
             var startCharacter = e.range.start.character
