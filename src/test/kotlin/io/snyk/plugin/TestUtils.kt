b6ddd0e71107a11b475c2a588aee48c5b57dcc5b
Bastian Doetsch <bastian.doetsch@snyk.io>
2025-06-02 15:03:00 +0200

    chore: re-introduce replaceService
    it was hidden in the testframework dependencies.

--- src/test/kotlin/io/snyk/plugin/TestUtils.kt
+++ src/test/kotlin/io/snyk/plugin/TestUtils.kt
@@ -1,11 +1,15 @@
 package io.snyk.plugin
 
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
+import com.intellij.testFramework.replaceService
 import com.intellij.util.io.RequestBuilder
 import io.mockk.every
 import io.mockk.justRun
 import io.mockk.mockk
 import io.mockk.mockkObject
+import io.snyk.plugin.services.SnykApplicationSettingsStateService
+import io.snyk.plugin.services.SnykProjectSettingsStateService
 import io.snyk.plugin.services.download.HttpRequestHelper
 import io.snyk.plugin.services.download.HttpRequestHelper.createRequest
 import snyk.common.lsp.LanguageServerWrapper
@@ -28,8 +32,17 @@ fun removeDummyCliFile() {
 }
 
 fun resetSettings(project: Project?) {
-    val settingsService = pluginSettings()
-    settingsService.reset()
+    val application = ApplicationManager.getApplication()
+    application.replaceService(
+        SnykApplicationSettingsStateService::class.java,
+        SnykApplicationSettingsStateService(),
+        application
+    )
+    project?.replaceService(
+        SnykProjectSettingsStateService::class.java,
+        SnykProjectSettingsStateService(),
+        project
+    )
     try {
         LanguageServerWrapper.getInstance(project!!).shutdown()
     } catch (_: Exception) {
