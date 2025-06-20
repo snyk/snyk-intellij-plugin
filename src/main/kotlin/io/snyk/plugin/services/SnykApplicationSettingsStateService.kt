2749e0bb68b14409acc384ab67a7c8aa5af5ee79
Andrew Robinson Hodges <andrew.robinsonhodges@snyk.io>
2025-06-20 17:13:18 +0100

    fix: revert renaming oauth authentication type from oauth2

--- src/main/kotlin/io/snyk/plugin/services/SnykApplicationSettingsStateService.kt
+++ src/main/kotlin/io/snyk/plugin/services/SnykApplicationSettingsStateService.kt
@@ -185,7 +185,7 @@ class SnykApplicationSettingsStateService : PersistentStateComponent<SnykApplica
 }
 
 enum class AuthenticationType(val languageServerSettingsName: String, val dialogName: String, val dialogIndex: Int) {
-    OAUTH2("oauth2", "OAuth2 (Recommended)", 0),
+    OAUTH2("oauth", "OAuth2 (Recommended)", 0),
     PAT("pat", "Personal Access Token", 1),
     API_TOKEN("token", "API Token (Legacy)", 2),
 }
